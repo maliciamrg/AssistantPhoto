7a67db6602a7d0a03934ac94a3e78ebb736b1f77
cleme <malcia.mrg@gmail.com>
2024-11-06 17:13:03 +0100

    commit initial

---
+++ AssistantPhoto/src/main/java/com/malicia/mrg/assistant/photo/AssistantPhotoApplication.java
@@ -0,0 +1,13 @@
+package com.malicia.mrg.assistant.photo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssistantPhotoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssistantPhotoApplication.class, args);
+	}
+
+}
