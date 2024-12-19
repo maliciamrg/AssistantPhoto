4511c94ac71dc0d5817496446629a5816034d3e8
cleme <malcia.mrg@gmail.com>
2024-12-19 23:47:24 +0100

    mock

--- frontend/src/mocks/browser.js
+++ frontend/src/mocks/browser.js
@@ -1,4 +1,6 @@
-import { setupWorker } from 'msw/browser'
-import { handlers } from './handlers'
+// src/mocks/browser.js
+import { setupWorker } from 'msw';
+import { handlers } from './handlers';
 
-export const worker = setupWorker(...handlers)
+// Set up the worker with the handlers
+export const worker = setupWorker(...handlers);
